6fef2a06736b01c39c92bda587d41895b56b9f1c
SimonTheoret <simonteoret@hotmail.com>
2023-11-28 16:59:27 -0500

    gans init

---
+++ practical/gan_solution.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn as nn
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# Training Hyperparameters
+batch_size = 64   # Batch Size
+z_dim = 32        # Latent Dimensionality
+gen_lr = 1e-4     # Learning Rate for the Generator
+disc_lr = 1e-4    # Learning Rate for the Discriminator/
+# Define Dataset Statistics
+image_size = 32
+input_channels = 1
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels, generator_features=32):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, generator_features * 4, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(generator_features * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(generator_features * 4, generator_features * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(generator_features * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( generator_features * 2, generator_features * 1, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(generator_features * 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( generator_features * 1, channels, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+        self.apply(weights_init)
+
+    def forward(self, input):
+        return self.model(input)
+
+class Discriminator(nn.Module):
+    def __init__(self, channels, discriminator_features=32):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(channels, discriminator_features, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(discriminator_features, discriminator_features * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(discriminator_features * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(discriminator_features * 2, discriminator_features * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(discriminator_features * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(discriminator_features * 4, 1, 4, 1, 0, bias=False),
+        )
+
+        self.apply(weights_init)
+
+    def forward(self, input):
+        return self.model(input)
+
+generator = Generator(z_dim, input_channels).to(device)
+discriminator = Discriminator(input_channels).to(device)
+
+discriminator_optimizer = torch.optim.Adam(discriminator.parameters(),lr = disc_lr, betas=(0.5, 0.999))    # WRITE CODE HERE
+generator_optimizer = torch.optim.Adam(generator.parameters(),lr = gen_lr, betas=(0.5, 0.999))        # WRITE CODE HERE
+
+criterion = torch.nn.BCELoss()    # WRITE CODE HERE
+
+def discriminator_train(discriminator, generator, real_samples, fake_samples):
+  # Takes as input real and fake samples and returns the loss for the discriminator
+  # Inputs:
+  #   real_samples: Input images of size (batch_size, 3, 32, 32)
+  #   fake_samples: Input images of size (batch_size, 3, 32, 32)
+  # Returns:
+  #   loss: Discriminator loss
+
+  ones = None   # WRITE CODE HERE (targets for real data)
+  zeros = None  # WRITE CODE HERE (targets for fake data)
+
+  real_output = None    # WRITE CODE HERE (output of discriminator on real data)
+  fake_output = None    # WRITE CODE HERE (output of discriminator on fake data)
+
+  loss = None           # WRITE CODE HERE (define the loss based on criterion and above variables)
+
+  return loss
+
+def generator_train(discriminator, generator, fake_samples):
+  # Takes as input fake samples and returns the loss for the generator
+  # Inputs:
+  #   fake_samples: Input images of size (batch_size, 3, 32, 32)
+  # Returns:
+  #   loss: Generator loss
+
+  ones = None   # WRITE CODE HERE (targets for fake data but for generator loop)
+
+  output = None # WRITE CODE HERE (output of the discriminator on the fake data)
+
+  loss = None   # WRITE CODE HERE (loss for the generator based on criterion and above variables)
+
+  return loss
+
+def sample(generator, num_samples, noise=None):
+  # Takes as input the number of samples and returns that many generated samples
+  # Inputs:
+  #   num_samples: Scalar denoting the number of samples
+  # Returns:
+  #   samples: Samples generated; tensor of shape (num_samples, 3, 32, 32)
+
+  with torch.no_grad():
+    # WRITE CODE HERE (sample from p_z and then generate samples from it)
+    pass
+
+
+def interpolate(generator, z_1, z_2, n_samples):
+  # Interpolate between z_1 and z_2 with n_samples number of points, with the first point being z_1 and last being z_2.
+  # Inputs:
+  #   z_1: The first point in the latent space
+  #   z_2: The second point in the latent space
+  #   n_samples: Number of points interpolated
+  # Returns:
+  #   sample: A sample from the generator obtained from each point in the latent space
+  #           Should be of size (n_samples, 3, 32, 32)
+
+  # WRITE CODE HERE (interpolate z_1 to z_2 with n_samples points and then)
+  # WRITE CODE HERE (    generate samples from the respective latents     )
+
+  return None
